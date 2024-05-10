b46c2a2df8309382d4fe4ebeb155ab5c845d52e4
VefaKazimova <bd77E7FT4@code.edu.az>
2024-05-10 14:34:36 +0400

    file is modified

--- src/pages/Basket/index.jsx
+++ src/pages/Basket/index.jsx
@@ -5,6 +5,7 @@ import { FaRegTrashAlt } from "react-icons/fa";
 import { CiCirclePlus } from "react-icons/ci";
 import { CiCircleMinus } from "react-icons/ci";
 import NotFound from "../../Components/NotFound";
+import { decremented } from "../../Slice";
 
 const Basket = () => {
   const values = useSelector((state) => state.baskets);
@@ -14,7 +15,17 @@ const Basket = () => {
   const calculateTotal = (price, quantity) => {
     return price * quantity;
   };
-
+  const removeToCard = ({ id, name, imgUrl, price }) => {
+    dispatch(
+      decremented({
+        id,
+        name,
+        imgUrl,
+        price,
+      })
+    );
+    console.log(removeToCard);
+  };
   const handleIncrement = (id) => {
     setQuantities((prevQuantities) => ({
       ...prevQuantities,
@@ -87,61 +98,70 @@ const Basket = () => {
             </tr>
           </thead>
           <tbody className="bg-white ">
-            {values.map(({ id, name, imgUrl, price }) => (
-              <tr key={id}>
-                <td className="px-6 py-4 ">
-                  <div className="flex items-center ">
-                    <div className=" h-10 w-10">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src={imgUrl}
-                        alt={name}
-                      />
-                    </div>
-                  </div>
-                </td>
-                <td className="px-6 py-4 ">
-                  <div className="text-sm font-medium text-gray-900">
-                    {name}
-                  </div>
-                </td>
-                <td className="px-6 py-4 ">
-                  <div className="text-sm text-gray-900">{price}</div>
-                </td>
-                <td className="px-6 py-4 ">
-                  <div className="text-sm text-gray-900 flex items-center gap-5">
-                    <div>
-                      <button onClick={() => handleIncrement(id)}>
-                        <CiCirclePlus />
-                      </button>
-                    </div>
-                    <div>
-                      <span>{quantities[id] || 1}</span>
-                    </div>
-                    <div>
-                      <button onClick={() => handleDecrement(id)}>
-                        <CiCircleMinus />
-                      </button>
-                    </div>
-                  </div>
-                </td>
+            {values.map((prod) => {
+              const { id, imgUrl, name, price } = prod;
 
-                <td className="px-6 py-4 ">
-                  <div
-                    before="$"
-                    after="USD"
-                    className="text-sm text-gray-900 before:content-[attr(before)] after:content-[attr(after)] after:m-1 before:mr-1"
-                  >
-                    {calculateTotal(price, quantities[id] || 1)}
-                  </div>
-                </td>
-                <td className="px-6 py-4   ">
-                  <button className="text-red-700 hover:text-red-900">
-                    <FaRegTrashAlt />
-                  </button>
-                </td>
-              </tr>
-            ))}
+              return (
+                <tr key={id}>
+                  <td className="px-6 py-4 ">
+                    <div className="flex items-center ">
+                      <div className=" h-10 w-10">
+                        <img
+                          className="h-10 w-10 rounded-full"
+                          src={imgUrl}
+                          alt={name}
+                        />
+                      </div>
+                    </div>
+                  </td>
+                  <td className="px-6 py-4 ">
+                    <div className="text-sm font-medium text-gray-900">
+                      {name}
+                    </div>
+                  </td>
+                  <td className="px-6 py-4 ">
+                    <div className="text-sm text-gray-900">{price}</div>
+                  </td>
+                  <td className="px-6 py-4 ">
+                    <div className="text-sm text-gray-900 flex items-center gap-5">
+                      <div>
+                        <button onClick={() => handleIncrement(id)}>
+                          <CiCirclePlus />
+                        </button>
+                      </div>
+                      <div>
+                        <span>{quantities[id] || 1}</span>
+                      </div>
+                      <div>
+                        <button onClick={() => handleDecrement(id)}>
+                          <CiCircleMinus />
+                        </button>
+                      </div>
+                    </div>
+                  </td>
+
+                  <td className="px-6 py-4 ">
+                    <div
+                      before="$"
+                      after="USD"
+                      className="text-sm text-gray-900 before:content-[attr(before)] after:content-[attr(after)] after:m-1 before:mr-1"
+                    >
+                      {calculateTotal(price, quantities[id] || 1)}
+                    </div>
+                  </td>
+                  <td className="px-6 py-4   ">
+                    <button
+                      onClick={() => {
+                        removeToCard(prod);
+                      }}
+                      className="text-red-700 hover:text-red-900"
+                    >
+                      <FaRegTrashAlt />
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
